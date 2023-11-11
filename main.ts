275c39f5bb385ba9bd49933db2ec5dede3782040
pocomillythescratcher <143274784+pocomillythescratcher@users.noreply.github.com>
2023-11-11 12:28:41 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,31 @@
+let REZULTAT = ""
 let directie = 0
+let tinta = ""
+let pc_cardinale = [
+"N",
+"E",
+"S",
+"V"
+]
 basic.forever(function () {
+    tinta = pc_cardinale._pickRandom()
+    basic.showString("" + (tinta))
+    basic.pause(5000)
     directie = input.compassHeading()
     if (directie < 45) {
-        basic.showString("N")
+        REZULTAT = "N"
     } else if (directie < 135) {
-        basic.showString("E")
+        REZULTAT = "E"
     } else if (directie < 225) {
-        basic.showString("S")
+        REZULTAT = "S"
     } else if (directie < 315) {
-        basic.showString("V")
+        REZULTAT = "V"
     } else {
-        basic.showString("N")
+        REZULTAT = "N"
+    }
+    if (REZULTAT == tinta) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.Sad)
     }
 })
